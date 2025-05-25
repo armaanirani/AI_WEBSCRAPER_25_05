207034d9c41f7a7a51acf34ab0b52545e85c56ef
Armaan Irani <armaanirani@gmail.com>
2025-05-25 13:46:05 +0530

    updated chrome driver path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from scrape import scrape_website
 
 
 st.title("AI Web Scraper")
@@ -6,3 +7,5 @@ url = st.text_input("Enter the URL to scrape:")
 
 if st.button("Scrape Site"):
     st.write(f"Scraping the site: {url}")
+    result = scrape_website(url)
+    print(result)
