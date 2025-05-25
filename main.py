55721853bbad8c1c832342d90a7a43bf983259d2
Armaan Irani <armaanirani@gmail.com>
2025-05-25 15:10:27 +0530

    integrated the dom content extraction and added a text box to interact with llm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import scrape_website, split_dom_content, clean_body_content, extract_body_content
 
 
 st.title("AI Web Scraper")
@@ -9,7 +9,22 @@ if st.button("Scrape Site"):
     st.write(f"Scraping the site: {url}")
     try:
         result = scrape_website(url)
-        st.write("Scraping successful! Here's the page content:")
-        st.code(result[:1000])  # Show first 1000 chars to avoid overwhelming UI
+        body_content = extract_body_content(result)
+        cleaned_content = clean_body_content(body_content)
+        
+        st.session_state.dom_content = cleaned_content
+        
+        with st.expander("View DOM Content"):
+            st.text_area("DOM Content", value=cleaned_content, height=300)
     except Exception as e:
         st.error(f"Scraping failed: {str(e)}")
+
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse?")
+    
+    if st.button("Parse DOM Content"):
+        if parse_description:
+            st.write("Parsing the content")
+            
+            dom_chunks = split_dom_content(st.session_state.dom_content)
